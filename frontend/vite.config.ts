23a1a4c2c9d0f9c82154e30fe8074b8b65d1a9f3
Saud Sheik <143426268+saud-sheik@users.noreply.github.com>
2026-07-13 14:03:48 +0530

    sales man #2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:5506',
+        target: 'http://127.0.0.1:5505',
         changeOrigin: true,
         secure: false,
       }
