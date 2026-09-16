8413b0fddd7918025a5437d4aec643ffc5aaf0a6
saud-sheik <sheiksaud671@gmail.com>
2026-09-16 14:26:08 +0530

    feat(frontend): add Team KPI Performance page showing gap-to-target per salesperson

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ import { SalesLayout } from "./components/SalesLayout";
 import ManagementDashboard from "./pages/ManagementDashboard";
 import MyDashboard from "./pages/MyDashboard";
 import KpiDashboard from "./pages/KpiDashboard";
+import TeamKpiPerformance from "./pages/TeamKpiPerformance";
 import SalesQueue from "./pages/SalesQueue";
 import LeadInbox from "./pages/LeadInbox";
 import LeadDetail from "./pages/LeadDetail";
@@ -93,6 +94,7 @@ function App() {
               <Route path="/home" element={<MyDashboard />} />
               <Route path="/inbox" element={<LeadInbox />} />
               <Route path="/kpi" element={<KpiDashboard />} />
+              <Route path="/kpi/team-performance" element={<TeamKpiPerformance />} />
               
               {/* Pre-Sales Leads Workspace */}
               <Route path="/leads" element={<Leads />} />
